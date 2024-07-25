394a483ed9e4465e848d8e3edc009263d1b4d8b7
Alfredocgn <alfredocgn@gmail.com>
2024-07-24 22:29:26 -0300

    new try

--- part2/phobebook/vite.config.js
+++ part2/phobebook/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://phonebook-ph1j.onrender.com/',
         changeOrigin: true,
       },
     }
